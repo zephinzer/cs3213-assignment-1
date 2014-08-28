5772c1b0d974c080f226bd21fb50ed10f8ba24ad
Joseph Matthias Goh <joseph@zephinzer.com>
2014-08-28 21:25:07 +0800

    Added submission for assignment inside /docs

--- ConsolePrint.java
+++ ConsolePrint.java
@@ -1,19 +1,7 @@
 import java.util.*;
 
 public class ConsolePrint{
-	
-	static Scanner userCommand = new Scanner(System.in);
-
-	public static void main(String[] args) {
-		run();
-	}
-	
-	private static void run(){
-		initialize();
-		runProgram();
-	}
-	
-	private static void initialize(){
+	public static void initialize(){
 		System.out.println("Hello " + System.getProperty("user.name") + "!\n"
 				+ "Select one of the option by typing the index:\n"
 				+ "1. Output the list of ignored words\n"
@@ -23,36 +11,30 @@ public class ConsolePrint{
 				
 	}
 	
-	private static void runProgram(){
-		KeyWordInContext ini = new KeyWordInContext();
-		String command = userCommand.next().replaceAll("[^1-4]", "");
-		
-		while(!command.isEmpty()){
-			switch(command){
-			
-			case "1":
-				 ini.printIgnoreWordArray();
-				 run();
-				break;
-			case "2":
-				ini.printTitleArray();
-				run();
-				break;
-			case "3":
-				ini.printProcessed();
-				run();
-				break;
-			case "4":
-				System.out.println("Exit now :)");
-				System.exit(0);
-				break;
-				default:
-					System.out.println("Something went wrong, please try again.");
-					System.exit(0);
-					
-			}
+	public void printIgnoreWordArray(ArrayList<String> ignoreWordsArray){
+		Integer tempLength = ignoreWordsArray.size();
+		for(Integer i = 0; i < tempLength; ++i) {
+			System.out.print(i+1 + ". ");
+			System.out.println(ignoreWordsArray.get(i));
 		}
-		
+		System.out.println("\n");
+	}
+	
+	public void printTitleArray(ArrayList<String> titleArray){
+		Integer tempLength = titleArray.size();
+		for(Integer i = 0; i < tempLength; ++i) {
+			System.out.print(i+1 + ". ");
+			System.out.println(titleArray.get(i));
+		}
+		System.out.println("\n");
+	}
+	
+	public void printProcessed(ArrayList<String> processedArray, int size){
+		for(int i = 0; i < size; i++){
+			System.out.print(i+1 + ". ");
+			System.out.println(processedArray.get(i));
+		}
+		System.out.println("\n");
 	}
 	
 	
--- KeyWordInContext.java
+++ KeyWordInContext.java
@@ -3,16 +3,19 @@ import java.util.*;
 public class KeyWordInContext {
 	Data title;
 	Data ignoreWords;
+	ConsolePrint view = new ConsolePrint();
+	Processor controller = new Processor();
+	
 	public static String pathToIgnoredWordsFile = System.getProperty("user.dir")+"\\_ignored-words.dat";//"C:\\Users\\CS\\Documents\\testFile02.txt";
 	public static String pathToTitleFile = System.getProperty("user.dir")+"\\_titles.dat"; //"C:\\Users\\CS\\Documents\\testFile01.txt";
+	public static final String ERROR_FAIL_TO_LOAD_IGNORED_WORDS = "Something went wrong while loading the ignored words. Check if the file exists at " + pathToIgnoredWordsFile;
+	public static final String ERROR_FAIL_TO_LOAD_TITLE = "Something went wrong while loading the title words. Check if the file exists at " + pathToTitleFile;
+	public static final String ERROR_FAIL_TO_LOAD_PROCESS_DATA = "Something went wrong while processing the data. Our bad. That shouldn\'t have happened.";
+	public static final String ERROR_INVALID_ENTRY = "That was not a valid entry. Please try again!";
 	
 	boolean ignoreWordsLoadSuccessful = false;
 	boolean titleLoadSuccessful = false;
-	ArrayList<String> ignoreWordsArray = new ArrayList<String>();
-	ArrayList<String> titleArray = new ArrayList<String>();
-	
-	
-	
+
 	public KeyWordInContext() {
 		//System.out.println("BEGIN\n");
 		
@@ -32,61 +35,73 @@ public class KeyWordInContext {
 		
 		try {
 			this.ignoreWords.loadData();
-			ignoreWordsArray = this.ignoreWords.getData();
-			ignoreWordsLoadSuccessful = true;
+			this.ignoreWordsLoadSuccessful = true;
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
+			this.ignoreWordsLoadSuccessful = false;
 		}
 		
 		try {
 			this.title.loadData();
-			titleArray = this.title.getData();
-			titleLoadSuccessful = true;
+			this.titleLoadSuccessful = true;
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
+			this.titleLoadSuccessful = false;
 		}
 		
 		
 	//	System.out.println("\nEND");
 	}
 	
-	public void printIgnoreWordArray(){
-		if(ignoreWordsLoadSuccessful == true) {
-			Integer tempLength = ignoreWordsArray.size();
-			for(Integer i = 0; i < tempLength; ++i) {
-				System.out.print(i+1 + ". ");
-				System.out.println(ignoreWordsArray.get(i));
-			}
-		}
-		
-		System.out.println("\n");
-	}
-	
-	public void printTitleArray(){
-		if(titleLoadSuccessful == true) {
-			Integer tempLength = titleArray.size();
-			for(Integer i = 0; i < tempLength; ++i) {
-				System.out.print(i+1 + ". ");
-				System.out.println(titleArray.get(i));
-			}
-		}
-
-		System.out.println("\n");
-	}
-	
-	public void printProcessed(){
-		Processor abc = new Processor();
-		ArrayList<String> lalala = abc.getProcessed(titleArray,ignoreWordsArray);
-		
-		for(int i = 0; i < abc.getPostProcessedTitleSize(); i++){
-			System.out.print(i+1 + ". ");
-			System.out.println(lalala.get(i));
-		}
-
-		System.out.println("\n");
-	}
-	
 	public static void main(String[] args) {
-		ConsolePrint.main(args);
+		Scanner userCommand = new Scanner(System.in);
+		KeyWordInContext ini = new KeyWordInContext();
+		
+		ini.view.initialize();
+		String command = userCommand.next().replaceAll("[^1-4]", "");
+		while(!command.isEmpty()){
+			switch(command){
+			
+			case "1":
+				try {
+					if(ini.ignoreWordsLoadSuccessful == true) {
+						ini.view.printIgnoreWordArray(ini.ignoreWords.getData());
+						main(args);
+				}
+				} catch (Exception ex) {
+					System.out.println(KeyWordInContext.ERROR_FAIL_TO_LOAD_IGNORED_WORDS);
+				}
+				break;
+			case "2":
+				try{
+					if(ini.titleLoadSuccessful == true) {
+						ini.view.printTitleArray(ini.title.getData());
+					}
+				} catch (Exception ex) {
+					System.out.println(KeyWordInContext.ERROR_FAIL_TO_LOAD_TITLE);
+				}
+				main(args);
+				break;
+			case "3":
+				try {
+					ini.view.printProcessed(
+						ini.controller.getProcessed(ini.title.getData(), ini.ignoreWords.getData()),
+						ini.controller.getPostProcessedTitleSize()
+					);
+				} catch (Exception ex) {
+					System.out.println(KeyWordInContext.ERROR_FAIL_TO_LOAD_PROCESS_DATA);
+				}
+				main(args);
+				break;
+			case "4":
+				System.out.println("Exit now :)");
+				System.exit(0);
+				break;
+			default:
+				System.out.println(KeyWordInContext.ERROR_INVALID_ENTRY);
+				System.exit(0);
+				break;
+			}
+		}
 	}
 }
